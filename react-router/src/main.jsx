d6da5720f2328d33bb4551be6d87f205c3d50711
DawoodIfzaal <dawoodifzaalpr@gmail.com>
2025-06-28 14:26:18 +0530

    add components and install react router

--- react-router/src/main.jsx
+++ react-router/src/main.jsx
@@ -1,10 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <h1 className='text-3xl text-cyan-800'> React Router </h1>
   </StrictMode>,
 )
